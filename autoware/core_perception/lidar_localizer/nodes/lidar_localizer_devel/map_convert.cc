47581e4f426a75b590de01c10cfce1bff944cadf
Soaringls <upc_ls@163.com>
2020-12-31 10:38:59 +0800

    update mapping 2020-1231

--- autoware/core_perception/lidar_localizer/nodes/lidar_localizer_devel/map_convert.cc
+++ autoware/core_perception/lidar_localizer/nodes/lidar_localizer_devel/map_convert.cc
@@ -7,27 +7,40 @@
 #include <boost/algorithm/string/join.hpp>
 #include <boost/filesystem.hpp>
 
+typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
+void LoadPointCloud(const std::string filename, PointCloud::Ptr& cloud) {
+  LOG(INFO) << "load file:" << filename;
+  if (!boost::filesystem::exists(filename)) {
+    LOG(FATAL) << "the file:" << filename << " not exist!";
+    // return -1;
+  }
+  if (pcl::io::loadPCDFile(filename.c_str(), *cloud) == -1) {
+    LOG(ERROR) << "could not load the:" << filename;
+    // return -1;
+  }
+}
+
 int main(int argc, char** argv) {
   // convert acii pcd to binary pcd
+  LOG(INFO) << "main's argc:" << argc;
   if (argc < 2) {
     LOG(INFO) << "usage: <excutable file> acii'sPCDfile..";
     return -1;
   }
   std::string filename = argv[1];
-  LOG(INFO) << "load file:" << filename;
-  if (!boost::filesystem::exists(filename)) {
-    LOG(FATAL) << "the file:" << filename << " not exist!";
-    return -1;
-  }
   std::string output_filename = filename + "binary";
-  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(
-      new pcl::PointCloud<pcl::PointXYZI>);
-  if (pcl::io::loadPCDFile(filename.c_str(), *cloud) == -1) {
-    LOG(ERROR) << "could not load the:" << filename;
-    return -1;
+  PointCloud::Ptr cloud1(new PointCloud);
+  LoadPointCloud(filename, cloud1);
+
+  if (argc == 3) {
+    PointCloud::Ptr cloud2(new PointCloud);
+    std::string filename2 = argv[2];
+    LoadPointCloud(filename2, cloud2);
+    *cloud1 += *cloud2;
   }
+
   LOG(INFO) << "load success, begin to save cloud to binary format..";
-  pcl::io::savePCDFileBinary(output_filename, *cloud);  // unity3D support
+  pcl::io::savePCDFileBinary(output_filename, *cloud1);  // unity3D support
   // pcl::io::savePCDFileBinaryCompressed(output_filename, *cloud);
   // pcl::io::savePCDFileASCII1(filename, *map_filtered);
   LOG(INFO) << "save success.";
---
+++ hengtong_demo/readme.md
@@ -0,0 +1,10 @@
+## quick start
+### load pointcloud map and vector map
+```
+修改hengtong_demo/launch/map.launch中hengtong_tf_local.launch、pointcloudmap、vectormap路径
+```
+/autoware/workspace/src/hengtong_demo/launch/map.launch
+
+```
+修改lidar_matching.launch中map_init.txt路径
+```
